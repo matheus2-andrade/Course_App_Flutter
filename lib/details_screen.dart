858e579cfe25cb070a822ce9150886553816d829
matheus2_andrade <matheus.moura@nutrin.com.br>
2021-09-22 08:56:25 -0300

    finish first row off detail screen

--- lib/details_screen.dart
+++ lib/details_screen.dart
@@ -9,40 +9,93 @@ class DetailsScreen extends StatelessWidget {
         body: Container(
       width: double.infinity,
       decoration: BoxDecoration(
+        color: Color(0xFFF5F4EF),
         image: DecorationImage(
           image: AssetImage("assets/images/ux_big.png"),
           alignment: Alignment.topRight,
         ),
       ),
-      child: Padding(
-        padding: EdgeInsets.only(left: 20, top: 50, right: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      child: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.only(left: 20, top: 50, right: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                SvgPicture.asset("assets/icons/arrow-left.svg"),
-                SvgPicture.asset("assets/icons/more-vertical.svg"),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    SvgPicture.asset("assets/icons/arrow-left.svg"),
+                    SvgPicture.asset("assets/icons/more-vertical.svg"),
+                  ],
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                ClipPath(
+                  clipper: BestSellerClipper(),
+                  child: Container(
+                    color: kBestSellerColor,
+                    padding:
+                        EdgeInsets.only(left: 10, top: 5, right: 20, bottom: 5),
+                    child: Text(
+                      "Mais Vendidos".toUpperCase(),
+                      style: TextStyle(fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Text(
+                  "Design Thinking",
+                  style: kHeadingextStyle,
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Row(
+                  children: [
+                    SvgPicture.asset("assets/icons/person.svg"),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Text("18K"),
+                    SvgPicture.asset("assets/icons/star.svg"),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Text("4.8")
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                RichText(
+                  text: TextSpan(children: [
+                    TextSpan(
+                      text: "\$50",
+                      style: kHeadingextStyle.copyWith(fontSize: 32),
+                    ),
+                    TextSpan(
+                      text: "\$70",
+                      style: TextStyle(
+                          color: kTextColor.withOpacity(.5),
+                          decoration: TextDecoration.lineThrough),
+                    ),
+                  ]),
+                ),
               ],
             ),
-            SizedBox(
-              height: 30,
-            ),
-            ClipPath(
-              clipper: BestSellerClipper(),
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          Expanded(
               child: Container(
-                color: kBestSellerColor,
-                padding:
-                    EdgeInsets.only(left: 10, top: 5, right: 20, bottom: 5),
-                child: Text(
-                  "Mais Vendidos".toUpperCase(),
-                  style: TextStyle(fontWeight: FontWeight.w600),
-                ),
-              ),
-            ),
-          ],
-        ),
+            color: Colors.white,
+          ))
+        ],
       ),
     ));
   }
